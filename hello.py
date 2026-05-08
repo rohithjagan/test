7a72b3b195b5eac0844a7f25b5f0666b5f9cc8f2
rohithjagan <rohithjagan25@gmail.com>
2026-05-08 16:54:32 +0530

    Update hello.py

--- hello.py
+++ hello.py
@@ -1,7 +1,1 @@
-# hello.py (modified)
-import sqlite3
-
-def get_user(user_id):
-    conn = sqlite3.connect('users.db')
-    query = f"SELECT * FROM users WHERE id = {user_id}"   # SQL injection
-    return conn.execute(query).fetchall()
+print("hello")
