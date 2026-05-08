988b1079401349fff04756ebc38a96d41c4e911a
rohithjagan <rohithjagan25@gmail.com>
2026-05-08 16:52:13 +0530

    Add function to get user data from SQLite database
    Added a function to retrieve user data from the database.

--- hello.py
+++ hello.py
@@ -1,2 +1,7 @@
-print("hello, world");
-print("hi")
+# hello.py (modified)
+import sqlite3
+
+def get_user(user_id):
+    conn = sqlite3.connect('users.db')
+    query = f"SELECT * FROM users WHERE id = {user_id}"   # SQL injection
+    return conn.execute(query).fetchall()
